b2e8d600803d849c39059633870a950a22db68f6
Brodi Veaudry <brodiv1217@gmail.com>
2026-04-08 11:22:19 +0200

    Commit 2
    Added variables and initialised variables

--- src/main/java/com/mycompany/securitysystem/User.java
+++ src/main/java/com/mycompany/securitysystem/User.java
@@ -1,13 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.securitysystem;
 
-/**
- *
- * @author Brodi
- */
+
 public class User {
-    
+    String username;
+    String pin;
+    int role;
+    String timestamp;
+    boolean isLocked;
+    int failedAttempts;
+
+    public User(String username, String pin, int role, String timestamp) {
+        this.username = username;
+        this.pin = pin;
+        this.role = role;
+        this.timestamp = timestamp;
+        this.isLocked = false;
+        this.failedAttempts = 0;
+    }
 }
